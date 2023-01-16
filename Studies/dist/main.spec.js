d03c695db146a8f2d55332bff6ed7e86ca8dfb95
Silva <john.silva@capgemini.com>
2023-01-16 18:16:38 -0300

    Finishing up module 4

--- Studies/dist/main.spec.js
+++ Studies/dist/main.spec.js
@@ -1,20 +1,27 @@
 'use strict';
 
 describe('main.js', function () {
-    let calculate1 = new Calculate();
+    beforeAll(function () {
+        this.calculate1 = new Calculate();
+        this.valuePassedIn = '';
+    });
 
-    console.log('main.js, calculate1', calculate1);
+    console.log('main.js, calculate1');
 
     describe('Calculate class', function () {
+        beforeEach(function() {
+            this.calculate1.resetTotal();
+        });
+
         describe('resetTotal()', function () {
             it('Should reset total', function () {
-                calculate1.total = 5;
-                console.log('calculate1.total', calculate1.total);
+                this.calculate1.total = 5;
+                console.log('this.calculate1.total', this.calculate1.total);
 
-                calculate1.resetTotal();
-                console.log('calculate1.total AF', calculate1.total);
+                this.calculate1.resetTotal();
+                console.log('this.calculate1.total AF', this.calculate1.total);
 
-                expect(calculate1.total).toBe(0);
+                expect(this.calculate1.total).toBe(0);
             });
         });
 
@@ -24,21 +31,22 @@ describe('main.js', function () {
                     numberA: 9,
                     numberB: 2
                 };
-                let valuePassedIn = '9+2';
 
-                console.log('handleInputValue() : calculate1.numbers', calculate1.numbers);
-                console.log('handleInputValue() : calculate1.specialChars', calculate1.specialChars);
+                this.valuePassedIn = '9+2';
 
-                calculate1.numbers = {};
-                calculate1.specialChars = '';
+                console.log('handleInputValue() : this.calculate1.numbers', this.calculate1.numbers);
+                console.log('handleInputValue() : this.calculate1.specialChars', this.calculate1.specialChars);
 
-                calculate1.handleInputValue(valuePassedIn);
+                this.calculate1.numbers = {};
+                this.calculate1.specialChars = '';
 
-                console.log('handleInputValue() : calculate1.numbers AFT', calculate1.numbers);
-                console.log('handleInputValue() : calculate1.specialChars AFT', calculate1.specialChars);
+                this.calculate1.handleInputValue(this.valuePassedIn);
 
-                expect(calculate1.numbers).toEqual(numberObj)
-                expect(calculate1.specialChars).toMatch(calculate1.expression);
+                console.log('handleInputValue() : this.calculate1.numbers AFT', this.calculate1.numbers);
+                console.log('handleInputValue() : this.calculate1.specialChars AFT', this.calculate1.specialChars);
+
+                expect(this.calculate1.numbers).toEqual(numberObj)
+                expect(this.calculate1.specialChars).toMatch(this.calculate1.expression);
             });
         });
 
@@ -48,8 +56,9 @@ describe('main.js', function () {
                     numberA: 5,
                     numberB: 3
                 };
-                let valuePassedIn = '5+3';
-                let calcNumberObj = calculate1.getNumbers(valuePassedIn);
+                this.valuePassedIn = '5+3';
+
+                let calcNumberObj = this.calculate1.getNumbers(this.valuePassedIn);
 
                 expect(calcNumberObj).toEqual(numberObj);
             });
@@ -57,38 +66,119 @@ describe('main.js', function () {
 
         describe('getSpecialChar()', function () {
             it('Should get only special characters, such as +, -, / and *', function () {
-                let valuePassedIn = '5+3';
-                let calcNumberObj = calculate1.getSpecialChar(valuePassedIn);
+                this.valuePassedIn = '5+3';
 
-                expect(calcNumberObj).toMatch(calculate1.expression);
+                let calcNumberObj = this.calculate1.getSpecialChar(this.valuePassedIn);
+
+                expect(calcNumberObj).toMatch(this.calculate1.expression);
             });
         });
 
         describe('addInitialValue()', function () {
             it('Should add the initial value from the object numbers.numberA entry', function () {
-                let valuePassedIn = '5+3';
+                this.valuePassedIn = '5+3';
 
-                console.log('calculate1.total BF', calculate1.total);
+                console.log('calculate1.total BF', this.calculate1.total);
 
-                calculate1.handleInputValue(valuePassedIn)
-                calculate1.addInitialValue();
+                this.calculate1.handleInputValue(this.valuePassedIn)
+                this.calculate1.addInitialValue();
 
-                console.log('calculate1.total AF', calculate1.total);
+                console.log('this.calculate1.total AF', this.calculate1.total);
 
-                expect(calculate1.total).toBe(5);
+                expect(this.calculate1.total).toBe(5);
             });
         });
 
         describe('validateInput()', function () {
             it('Should add the initial value from the object numbers.numberA entry', function () {
-                let valuePassedIn = '5+3';
+                this.valuePassedIn = '5+3';
 
-                console.log('calculate1.total BF', calculate1.total);
+                console.log('this.calculate1.total BF', this.calculate1.total);
 
-                calculate1.handleInputValue(valuePassedIn)
+                this.calculate1.handleInputValue(this.valuePassedIn);
 
-                expect(calculate1.validateInput()).toBeTruthy();
+                expect(this.calculate1.validateInput()).toBeTruthy();
             });
         });
-    })
+
+        describe('handleOperation()', function () {
+            describe('Switch operation', function () {
+                beforeEach(function () {
+                    this.calculate1.resetTotal();
+                });
+
+                it('Should add value based on the value passed in', function () {
+                    this.valuePassedIn = '5+3';
+
+                    this.calculate1.handleInputValue(this.valuePassedIn).addInitialValue();
+
+                    console.log('this.calculate1.numbers.numberA', this.calculate1.numbers.numberA);
+                    console.log('this.calculate1.numbers.numberB', this.calculate1.numbers.numberB);
+
+                    this.calculate1.handleOperation();
+
+                    expect(this.calculate1.result).toBe(8);
+                });
+
+                it('Should subtract value based on the value passed in', function () {
+                    this.valuePassedIn = '5-3';
+
+                    this.calculate1.handleInputValue(this.valuePassedIn).addInitialValue();
+
+                    console.log('this.calculate1.numbers.numberA', this.calculate1.numbers.numberA);
+                    console.log('this.calculate1.numbers.numberB', this.calculate1.numbers.numberB);
+
+                    this.calculate1.handleOperation();
+
+                    expect(this.calculate1.result).toBe(2);
+                });
+
+                it('Should multiply value based on the value passed in', function () {
+                    this.valuePassedIn = '5*3';
+
+                    this.calculate1.handleInputValue(this.valuePassedIn).addInitialValue();
+
+                    console.log('calculate1.numbers.numberA', this.calculate1.numbers.numberA);
+                    console.log('this.calculate1.numbers.numberB', this.calculate1.numbers.numberB);
+
+                    this.calculate1.handleOperation();
+
+                    expect(this.calculate1.result).toBe(15);
+                });
+
+                it('Should divide value based on the value passed in', function () {
+                    this.valuePassedIn = '6/2';
+
+                    this.calculate1.handleInputValue(this.valuePassedIn).addInitialValue();
+
+                    console.log('this.calculate1.numbers.numberA', this.calculate1.numbers.numberA);
+                    console.log('this.calculate1.numbers.numberB', this.calculate1.numbers.numberB);
+
+                    this.calculate1.handleOperation();
+
+                    expect(this.calculate1.result).toBe(3);
+                });
+            })
+        });
+
+        describe('updateResult()', function() {
+            beforeAll(function () {
+                const element = document.createElement('div');
+                element.setAttribute('id', 'testResult');
+                element.innerText = '5';
+
+                document.body.appendChild(element);
+
+                this.element = element;
+            });
+
+            afterAll(function () {
+                document.body.removeChild(this.element);
+            });
+
+            it('Should update the DOM with the calculator\'s result', function() {
+                expect(parseInt(this.element.innerText)).toBe(5);
+            });
+        });
+    });
 });
